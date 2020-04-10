b84b7ba588f817cb4a47fb990cd67e8953892231
Vincent Hoang <vietnhoang7@gmail.com>
2020-04-10 13:15:49 -0700

    Begin working on modal

---
+++ Tiledoku/BoardSizeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BoardSizeViewController.swift
+//  Tiledoku
+//
+//  Created by Vincent Hoang on 4/10/20.
+//  Copyright © 2020 Vincent Hoang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BoardSizeViewController: UIViewController {
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var largeButton: UIButton!
+    @IBAction func largeButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var mediumButton: UIButton!
+    @IBAction func mediumButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var smallButton: UIButton!
+    @IBAction func smallButtonPressed(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Tiledoku/MenuViewController.swift
+++ Tiledoku/MenuViewController.swift
@@ -34,6 +34,42 @@ class MenuViewController: UIViewController {
         return true
     }
     
+    @IBAction func openModalView(_ sender: Any) {
+        self.definesPresentationContext = true
+        self.providesPresentationContextTransitionStyle = true
+        
+        self.overlayBlurredBackgroundView()
+    }
+    
+    func overlayBlurredBackgroundView() {
+        let blurredBackgroundView = UIVisualEffectView()
+        
+        blurredBackgroundView.frame = view.frame
+        blurredBackgroundView.effect = UIBlurEffect(style: .dark)
+        
+        view.addSubview(blurredBackgroundView)
+    }
+    
+    func removeBlurredBackgroundView() {
+        
+        for subview in view.subviews {
+            if subview.isKind(of: UIVisualEffectView.self) {
+                subview.removeFromSuperview()
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "ShowModalView" {
+                if let viewController = segue.destination as? BoardSizeViewController {
+                    viewController.delegate = self
+                    viewController.modalPresentationStyle = .overFullScreen
+                }
+            }
+        }
+    }
+    
 }
 
 @IBDesignable extension UIButton {
